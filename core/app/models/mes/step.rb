cddb15eaa5d9742eccdeb5c93f772b13cb695872
Eric Guo <eric@cloud-mes.com>
2015-10-26 22:46:08 +0800

    Allow new step created with the new workflow.

--- core/app/models/mes/step.rb
+++ core/app/models/mes/step.rb
@@ -1,10 +1,10 @@
 module Mes
   class Step < ActiveRecord::Base
-    belongs_to :workflow
+    belongs_to :workflow, optional: true
     belongs_to :step_code
     delegate :name, :description, to: :step_code
 
     validates :step_code, presence: true, uniqueness: { scope: [:workflow_id] }
-    validates :workflow, presence: true
+    validates :workflow, presence: true, on: :update
   end
 end
