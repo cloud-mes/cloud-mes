8cb8743ad0447d8b9da3c0568f39aaaf97cedfd8
Eric Guo <eric@cloud-mes.com>
2015-10-30 14:20:34 +0800

    To avoid only show one.

--- modeler/app/helpers/mes/modeler/sidebar_menu_helper.rb
+++ modeler/app/helpers/mes/modeler/sidebar_menu_helper.rb
@@ -1,14 +1,14 @@
 module Mes::Modeler::SidebarMenuHelper
   def sub_menu_product_model(text, id:, icon: nil)
     sub_menu_tree text, id, icon: icon do
-      sidebar_menu_item(Mes.t(:products), mes.modeler_products_path) if policy(Mes::Product).index?
-      sidebar_menu_item(Mes.t(:workflows), mes.modeler_workflows_path) if policy(Mes::Workflow).index?
+      concat sidebar_menu_item(Mes.t(:products), mes.modeler_products_path) if policy(Mes::Product).index?
+      concat sidebar_menu_item(Mes.t(:workflows), mes.modeler_workflows_path) if policy(Mes::Workflow).index?
     end
   end
 
   def sub_menu_equipment(text, id:, icon: nil)
     sub_menu_tree text, id, icon: icon do
-      sidebar_menu_item(Mes.t(:machine_types), mes.modeler_machine_types_path) if policy(Mes::MachineType).index?
+      concat sidebar_menu_item(Mes.t(:machine_types), mes.modeler_machine_types_path) if policy(Mes::MachineType).index?
     end
   end
 
